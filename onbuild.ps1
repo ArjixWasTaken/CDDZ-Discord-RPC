87da0826322251c49cc9f5839e97776a4465c14f
ArjixWasTaken <53124886+ArjixWasTaken@users.noreply.github.com>
2023-05-21 22:51:29 +0300

    gg

---
+++ onbuild.ps1
@@ -0,0 +1,3 @@
+﻿$args[0] = '"' + $args[0] + '"'
+
+xcopy /y $args[0] "D:/Games/Clone Drone In The Danger Zone/BepInEx/plugins"
---
+++ run.ps1
@@ -0,0 +1,13 @@
+﻿Start-Process -FilePath "D:/Games/Clone Drone In The Danger Zone/Clone Drone in the Danger Zone.exe"
+Start-Sleep -Seconds 1
+
+$process = Get-Process -Name "Clone Drone in the Danger Zone"
+$_ = (New-Object -ComObject WScript.Shell).AppActivate($process.MainWindowTitle)
+
+while ($true)
+{
+    if ($process.HasExited) {
+        break;
+    }
+    Start-Sleep -Seconds 1
+}
